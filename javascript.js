d819da284a9ab7796e8fdbad42707cf80542483f
Dieter91 <dieter.v91@gmail.com>
2021-08-30 00:14:24 +0200

    skill titles are linked

--- javascript.js
+++ javascript.js
@@ -1,17 +1,15 @@
 let skillBar = document.getElementsByClassName("skill-bar");
 let desc = document.getElementsByClassName("desc");
 let skillsRight = document.getElementsByClassName("skills-right")[0];
-
+let skillTitle = document.getElementsByClassName("skill-title");
 for (let i = 0; i < skillBar.length; i++) {
   skillBar[i].addEventListener("click", function () {
     skillsRight.getElementsByClassName("active")[0].classList.remove("active");
     desc[i].classList.add("active");
   });
-}
 
-// skillBar.addEventListener("click", function () {
-//   //document.getElementsByClassName("html-desc")[0].style.display = "initial";
-//   document.getElementsByClassName(
-//     "skill-container"
-//   )[0].childNodes[5].style.display = "initial";
-// });
+  skillTitle[i].addEventListener("click", function () {
+    skillsRight.getElementsByClassName("active")[0].classList.remove("active");
+    desc[i].classList.add("active");
+  });
+}
