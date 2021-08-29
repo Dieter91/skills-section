437ee8f84a6d1c38a7f55037e6bbf7f1f275a8f2
Dieter91 <dieter.v91@gmail.com>
2021-08-30 00:18:07 +0200

    spacing

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let skillBar = document.getElementsByClassName("skill-bar");
 let desc = document.getElementsByClassName("desc");
 let skillsRight = document.getElementsByClassName("skills-right")[0];
 let skillTitle = document.getElementsByClassName("skill-title");
+
 for (let i = 0; i < skillBar.length; i++) {
   skillBar[i].addEventListener("click", function () {
     skillsRight.getElementsByClassName("active")[0].classList.remove("active");
